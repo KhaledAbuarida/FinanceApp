365c87eab1d86d490f7741558b9c63027b9b7588
Khaled Mahmoud Abuarida <khaledabuarida0@gmail.com>
2025-12-26 01:33:34 +0200

    Add project files.

---
+++ FinanceApp/Migrations/20251225232817_change type of expense.amount, expense.category.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinanceApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class changetypeofexpenseamountexpensecategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Category",
+                table: "Expenses",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Amount",
+                table: "Expenses",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Category",
+                table: "Expenses",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "Expenses",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
